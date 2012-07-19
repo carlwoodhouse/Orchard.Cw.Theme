ede39ddf020e7cc71c021e64fc4c96ee8a113515
Carl Woodhouse <nothing.is.forever@gmail.com>
2012-07-19 23:57:42 +0100

    its starting to look a lil sexy

---
+++ Scripts/cw.main.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+    // grid
+
+    var zoom = 1.2;
+    var move = -20;
+    //On mouse over those thumbnail
+    $('.grid .item').hover(function () {
+
+        //Set the width and height according to the zoom percentage
+        width = $('.item').width() * zoom;
+        height = $('.item').height() * zoom;
+
+        //Move and zoom the image
+        $(this).find('img').stop(false, true).stop().animate({ 'width': width, 'height': height, 'top': move, 'left': move }, { duration: 200 });
+
+        //Display the caption
+        $(this).find('div.caption').stop(false, true).fadeIn(200);
+    },
+        function () {
+            //Reset the image
+            $(this).find('img').stop(false, true).stop().animate({ 'width': $('.item').width(), 'height': $('.item').height(), 'top': '0', 'left': '0' }, { duration: 100 });
+
+            //Hide the caption
+            $(this).find('div.caption').stop(false, true).fadeOut(200);
+        });
+});
